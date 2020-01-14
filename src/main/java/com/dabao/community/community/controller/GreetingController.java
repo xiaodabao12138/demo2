597761f705add01bd6b5d6014287524e41c9a33d
xiaodabao12138 <645984829@qq.com>
2020-01-14 20:04:02 +0800

    bootstrap初体验

--- src/main/java/com/dabao/community/community/controller/GreetingController.java
+++ src/main/java/com/dabao/community/community/controller/GreetingController.java
@@ -5,12 +5,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+/**
+ * 项目开始的示例
+ */
 @Controller
 public class GreetingController {
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name") String name, Model model) {
         model.addAttribute("name", name);
-        return "greeting";
+        return "index";
     }
 }
